aa4f9c8d488ff08a995f7fa74c845a3da3bdb74d
Svirido <e.svirido@gmail.com>
2022-07-27 20:57:53 +0300

    class TheDate

--- app/src/main/java/com/svirido/running_plan/CalendarActivity.kt
+++ app/src/main/java/com/svirido/running_plan/CalendarActivity.kt
@@ -2,10 +2,36 @@ package com.svirido.running_plan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.svirido.running_plan.databinding.ActivityCalendarBinding
 
 class CalendarActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCalendarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calendar)
+        binding = ActivityCalendarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var theDate: TheDate
+        val listTheDate = arrayListOf<TheDate>()
+
+        binding.calendarView.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
+            Log.d("calendar", " $year  $month  $dayOfMonth")
+            theDate = TheDate(year, month, dayOfMonth)
+            listTheDate.add(theDate)
+        }
+
+        binding.button1.setOnClickListener {
+            binding.textView1.text = listTheDate[0].dayOfMonth.toString()
+        }
+
+        binding.button2.setOnClickListener {
+            binding.textView2.text = listTheDate[1].dayOfMonth.toString()
+        }
+
+
+
     }
 }
--- app/src/main/java/com/svirido/running_plan/MainActivity.kt
+++ app/src/main/java/com/svirido/running_plan/MainActivity.kt
@@ -7,7 +7,7 @@ import com.svirido.running_plan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
